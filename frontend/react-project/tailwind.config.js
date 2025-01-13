dd4589a4c6ec64f46fd04fdc0d10207281da79ed
Tanveer <tweme0600@gmail.com>
2025-01-13 22:32:04 +0530

    Fix: Updated tailwind.config.js to use ES module syntax

--- frontend/react-project/tailwind.config.js
+++ frontend/react-project/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar'
 export default {
   content: [
     "./index.html",
@@ -8,6 +9,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('tailwind-scrollbar'),
+    tailwindScrollbar
   ],
 }
